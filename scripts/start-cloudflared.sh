f007b37b086b58f8123d8c7bffad95ea7a79943b
vencis <vaclav.fiser@gmail.com>
2025-03-14 08:46:39 +0100

    Update start-cloudflared.sh

--- scripts/start-cloudflared.sh
+++ scripts/start-cloudflared.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Test DNS resolution before starting
-if ! cloudflared proxy-dns --port 5053 --upstream $DOH_DNS1 --upstream $DOH_DNS2 --test-upstream; then
+if ! cloudflared proxy-dns --port 5053 --upstream $DOH_DNS1 --upstream $DOH_DNS2; then
     echo "Error: Failed to resolve DNS using cloudflared"
     exit 1
 fi
